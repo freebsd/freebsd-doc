e31b85ccd6e0f10434205e81965c11959d7744ae
wosch <wosch@FreeBSD.org>
1998-08-04 17:24:18 +0000

    The  repository copy www/data  -> www/en is done.

--- data/news/clickable.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl -np
-# Copyright (c) Juli 1998 Wolfram Schneider <wosch@FreeBSD.org>. Berlin.
-#
-# clickable - Make URL clickable
-#
-# $Id: clickable.pl,v 1.1 1998-07-15 12:50:56 wosch Exp $
-
-s/</&lt;/g;
-s%((http|ftp)://[^\s"\)\>,;]+)%<A HREF="$1">$1</A>%gi;
