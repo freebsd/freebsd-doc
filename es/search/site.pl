1ce17c847fd14695f9534ccc127b5f62e65d2f9d
wosch <wosch@FreeBSD.org>
1999-02-08 19:26:12 +0000

    Imported the Spanish Web pages, except the
    	english sub directories: cgi, commercial, gallery, gifs
    	which are redundant and will never be translated
    
    and the
    
    	spanish sub directories: doc-es, docs-es
    
    Submitted by: "Jesus Rodriguez" <jesusr@ncsa.es>
    
    Fix Makefiles errors. `Make all install' runs fine.
    
    Currently, many the links to the sub directories cgi, commercial,
    gallery, gifs are broken. The links should point to the English
    orginal files.

---
+++ es/search/site.pl
@@ -0,0 +1,57 @@
+#!/usr/bin/perl
+# Copyright (c) June 1998 Wolfram Schneider <wosch@FreeBSD.org>, Berlin.
+#
+# site - create automatically a site map
+#
+# Format: <url> | <description>
+# An empty url begin a new section
+#
+# $Id: site.pl,v 1.1.1.1 1999-02-08 19:26:12 wosch Exp $
+
+
+# print a dl list
+# <dl><dt>foo</dt>
+#     <dd>bla,foo,bar</dd>
+# </dl> 
+
+sub dl {
+    $menu = 0;
+    print "<DL>\n";
+
+    while(<>) {
+	# ignore comments and empty lines
+	next if /^\s*#/;
+	next if /^\s*$/;
+	
+	chop;
+	($url, $description) = split('\|');
+	$description =~ s/^\s+//;
+	$description =~ s/\s+$//;
+	
+	# new section
+	if (!$url && $description) {
+	    # close last <dd>
+	    if ($menu) {
+		print "\n", "  </DD>\n", "\n";
+	    }
+
+	    $menu = 1;
+	    print " <DT><STRONG>", $description, "</STRONG></DT>\n";
+	    print "  <DD>\n";
+	} 
+
+	# entries for a section
+	elsif ($menu) {
+	    # a comma execpt for the last entry
+	    print ",\n" if ($menu > 1);
+
+	    print "   <A HREF=", '"', $url, '">', $description, "</A>";
+	    $menu++;
+	}
+    }
+
+    print "\n", "  </DD>\n";
+    print "</DL>\n";
+}
+
+&dl;
