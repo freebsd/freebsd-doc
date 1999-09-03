d4e3e1aa6e808cdcba7b025ed81a3a63cb37a659
nik <nik@FreeBSD.org>
1999-09-03 17:07:19 +0000

    New make(1) infrastructure.  Split out docproj.docbook.mk into a number of smaller doc.<foo>.mk files, reflecting the functionality they contain. Long overdue, kudos to the submitter for the carrying out the work.
    Also makes the files independent of the system include files that
    normally live in /usr/share/mk, making it easier for non-FreeBSD systems
    to download and build our docs (an important factor in making it easier
    to share our work with other projects).
    
    Finally, it (in theory) lets you build the docs with a r/o doc/ directory.
    
    Changes to the submitted files:
    
    doc.docbook.mk    The HTML generation depends on ${DSLHTML}, and the
                      print generation depends on ${DSLPRINT}.  Changing
                      these files will force a rebuild (which makes testing
                      changes a little easier).
    
                      Removed ${DOC}.doc target.  It's a hangover from when
                      I (mistakenly) thought that Jade could generate MS Word
                      .doc files.
    
                      Added support for using compress(1) to build .Z files
                      (been on my todo list for ages).
    
                      Fixed a couple of typos.
    
    Submitted by:   Neil Blakey-Milner <nbm@mithrandr.moria.org>

---
+++ share/mk/doc.project.mk
@@ -0,0 +1,74 @@
+#
+# $Id: doc.project.mk,v 1.1 1999-09-03 17:07:18 nik Exp $
+#
+# This include file <doc.project.mk> is the FreeBSD Documentation Project 
+# co-ordination make file.
+#
+# This file includes the other makefiles, which contain enough
+# knowledge to perform their duties without the system make files.
+#
+
+# ------------------------------------------------------------------------
+#
+# Document-specific variables:
+#
+#	DOC		This _must_ be set if there is a document to
+#			build.  It should be without prefix.
+#
+#	DOCFORMAT	Format of the document.  Defaults to docbook.
+#			docbook is also the only option currently.
+#
+# 	MAINTAINER	This denotes who is responsible for maintaining
+# 			this section of the project.  If unset, set to
+# 			doc@FreeBSD.org
+#
+
+# ------------------------------------------------------------------------
+#
+# User-modifiable variables:
+#
+#	PREFIX		Standard path to document-building applications
+#			installed to serve the documentation build
+#			process, usually by installing the docproj port
+#			or package.  Default is /usr/local
+#
+#	NOINCLUDEMK	Whether to include the standard BSD make files,
+#			or just to emulate them poorly.  Set this if you
+#			aren't on FreeBSD, or a compatible sibling.  By
+#			default is not set.
+#
+
+# ------------------------------------------------------------------------
+#
+# Make files included:
+#
+#	doc.install.mk	Installation specific information, including
+#			ownership and permissions.
+#
+#	doc.subdir.mk	Subdirectory related configuration, including
+#			handling "obj" builds.
+#
+# DOCFORMAT-specific make files, like:
+#
+#	doc.docbook.mk	Building and installing docbook documentation.
+#			Currently the only method.
+#
+
+# Document-specific defaults
+DOCFORMAT?=	docbook
+MAINTAINER?=	doc@FreeBSD.org
+
+# User-modifiable
+PREFIX?=	/usr/local
+PRI_LANG?=	en_US.ISO_8859-1
+
+# Format-specific configuration
+.if defined(DOC)
+.if ${DOCFORMAT} == "docbook"
+.include "doc.docbook.mk"
+.endif
+.endif
+
+# Subdirectory glue and ownership information.
+.include "doc.subdir.mk"
+.include "doc.install.mk"
