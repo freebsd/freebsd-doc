f49a96cf7e5ff5f0932a950ee86a8ed0c0a7ba71
nik <nik@ba62deef-0ea0-e111-9166-001e4f0fb9b1>
1999-09-03 17:07:19 +0000

    New make(1) infrastructure.  Split out docproj.docbook.mk into a number of smaller doc.<foo>.mk files, reflecting the functionality they contain. Long overdue, kudos to the submitter for the carrying out the work.
    Also makes the files independent of the system include files that
    normally live in /usr/share/mk, making it easier for non-FreeBSD systems
    to download and build our docs (an important factor in making it easier
    to share our work with other projects).
    
    Finally, it (in theory) lets you build the docs with a r/o doc/ directory.
    
    Changes to the submitted files:
    
    doc.docbook.mk    The HTML generation depends on ${DSLHTML}, and the
                      print generation depends on ${DSLPRINT}.  Changing
                      these files will force a rebuild (which makes testing
                      changes a little easier).
    
                      Removed ${DOC}.doc target.  It's a hangover from when
                      I (mistakenly) thought that Jade could generate MS Word
                      .doc files.
    
                      Added support for using compress(1) to build .Z files
                      (been on my todo list for ages).
    
                      Fixed a couple of typos.
    
    Submitted by:   Neil Blakey-Milner <nbm@mithrandr.moria.org>
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/doc/head@5500 ba62deef-0ea0-e111-9166-001e4f0fb9b1

---
+++ share/mk/doc.subdir.mk
@@ -0,0 +1,195 @@
+# Taken from:
+#	Id: bsd.subdir.mk,v 1.27 1999/03/21 06:43:40 bde
+#
+# $Id: doc.subdir.mk,v 1.1 1999-09-03 17:07:19 nik Exp $
+#
+# This include file <doc.subdir.mk> contains the default targets
+# for building subdirectories in the FreeBSD Documentation Project.
+#
+# For all of the directories listed in the variable SUBDIR, the
+# specified directory will be visited and the target made. There is
+# also a default target which allows the command "make subdir" where
+# subdir is any directory listed in the variable SUBDIR.
+#
+
+# ------------------------------------------------------------------------
+#
+# Document-specific variables:
+#
+#	SUBDIR			A list of subdirectories that should be
+#				built as well.  Each of the targets will
+#				execute the same target in the
+#				subdirectories.
+#
+#	COMPAT_SYMLINK		Create a symlink named in this variable
+#				to this directory, when installed.
+#
+#	ROOT_SYMLINKS		Create symlinks to the named directories
+#				in the document root, if the current
+#				language is the primary language (the
+#				PRI_LANG variable).
+#
+
+# ------------------------------------------------------------------------
+#
+# Provided targets:
+#
+#	install:
+#	package:
+#			Go down subdirectories and call these targets
+#			along the way, and then call the real target
+#			here.
+#
+#	clean:
+#			Remove files created by the build process.
+#
+#	cleandir:
+#			Remove the object directory, if any.
+#
+
+.if exists(${.CURDIR}/../Makefile.inc)
+.include "${.CURDIR}/../Makefile.inc"
+.endif
+
+# ------------------------------------------------------------------------
+#
+# Work out the language and encoding used for this document.
+#
+# Liberal default of maximum of 5 directories below to find it.
+#
+
+.if !defined(LANGCODE)
+LANGCODE:=	${.CURDIR}
+.for _ in 1 2 3 4 5
+.if !(${LANGCODE:H:T} == "doc")
+LANGCODE:=	${LANGCODE:H}
+.endif
+.endfor
+LANGCODE:=	${LANGCODE:T}
+.endif
+
+
+.if !target(install)
+install: afterinstall symlinks 
+afterinstall: realinstall
+realinstall: beforeinstall _SUBDIRUSE
+.endif
+
+package: realpackage symlinks
+realpackage: _SUBDIRUSE
+
+.if !defined(IGNORE_COMPAT_SYMLINK) && defined(COMPAT_SYMLINK)
+SYMLINKS+= ${DOCDIR} ${.CURDIR:T:ja_JP.eucJP=ja} ${COMPAT_SYMLINK:ja=ja_JP.eucJP}
+.endif
+
+.if defined(PRI_LANG) && defined(ROOT_SYMLINKS) && !empty(ROOT_SYMLINKS)
+.if ${PRI_LANG} == ${LANGCODE}
+.for _tmp in ${ROOT_SYMLINKS}
+SYMLINKS+= ${DOCDIR} ${LANGCODE:ja_JP.eucJP=ja}/${.CURDIR:T}/${_tmp} ${_tmp}
+.endfor
+.endif
+.endif
+
+.if !target(symlinks)
+symlinks:
+.if defined(SYMLINKS) && !empty(SYMLINKS)
+	@set `echo ${SYMLINKS}`; \
+	while : ; do \
+		case $$# in \
+			0) break;; \
+			[12]) echo "warn: empty SYMLINKS: $$1 $$2"; break;; \
+		esac; \
+		d=$$1; shift; \
+		l=$$1; shift; \
+		t=$$1; shift; \
+		if [ ! -e $${d}/$${l} ]; then \
+			${ECHO} "$${d}/$${l} doesn't exist, not linking"; \
+		else \
+			${ECHO} $${d}/$${t} -\> $${d}/$${l}; \
+			(cd $${d} && rm -f $${t}); \
+			(cd $${d} && ln -s $${l} $${t}); \
+		fi; \
+	done
+.endif
+.endif
+
+.for __target in beforeinstall afterinstall realinstall realpackage
+.if !target(${__target})
+${__target}:
+.endif
+.endfor
+
+_SUBDIRUSE: .USE
+.for entry in ${SUBDIR}
+	@${ECHO} "===> ${DIRPRFX}${entry}"
+	@(cd ${.CURDIR}/${entry} && \
+	${MAKE} ${.TARGET:S/realpackage/package/:S/realinstall/install/} DIRPRFX=${DIRPRFX}${entry}/ )
+.endfor
+
+.if !defined(NOINCLUDEMK)
+
+.include <bsd.obj.mk>
+.include <bsd.subdir.mk>
+
+.else
+
+.MAIN: all
+
+${SUBDIR}::
+	cd ${.CURDIR}/${.TARGET}
+	${MAKE} all
+
+.for __target in all cleandir lint objlink install
+.if !target(${__target})
+${__target}: _SUBDIRUSE
+.endif
+.endfor
+
+.if !target(obj)
+obj:	_SUBDIRUSE
+	@if ! test -d ${CANONICALOBJDIR}/; then \
+		mkdir -p ${CANONICALOBJDIR}; \
+		if ! test -d ${CANONICALOBJDIR}/; then \
+			${ECHO} "Unable to create ${CANONICALOBJDIR}."; \
+			exit 1; \
+		fi; \
+		${ECHO} "${CANONICALOBJDIR} created ${.CURDIR}"; \
+	fi
+.endif
+
+.if !target(objlink)
+objlink: _SUBDIRUSE
+	@if test -d ${CANONICALOBJDIR}/; then \
+		rm -f ${.CURDIR}/obj; \
+		ln -s ${CANONICALOBJDIR} ${.CURDIR}/obj; \
+	else \
+		echo "No ${CANONICALOBJDIR} to link to - do a make obj."; \
+	fi
+.endif
+
+.if !target(whereobj)
+whereobj:
+	@echo ${.OBJDIR}
+.endif
+
+cleanobj:
+	@if [ -d ${CANONICALOBJDIR}/ ]; then \
+		rm -rf ${CANONICALOBJDIR}; \
+	else \
+		cd ${.CURDIR} && ${MAKE} clean cleandepend; \
+	fi
+	@if [ -h ${.CURDIR}/obj ]; then rm -f ${.CURDIR}/obj; fi
+
+.if !target(clean)
+clean: _SUBDIRUSE
+.if defined(CLEANFILES) && !empty(CLEANFILES)
+	rm -f ${CLEANFILES}
+.endif
+.if defined(CLEANDIRS) && !empty(CLEANDIRS)
+	rm -rf ${CLEANDIRS}
+.endif
+.endif
+
+cleandir: cleanobj _SUBDIRUSE
+
+.endif # end of NOINCLUDEMK section
