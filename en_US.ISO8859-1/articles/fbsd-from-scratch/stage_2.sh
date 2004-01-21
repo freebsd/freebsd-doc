c16deeecca473827422ea30b6b7b0b0238d86bab
schweikh <schweikh@FreeBSD.org>
2004-01-21 19:39:26 +0000

    Minor improvements to all stages and documentation.

--- en_US.ISO8859-1/articles/fbsd-from-scratch/stage_2.sh
+++ en_US.ISO8859-1/articles/fbsd-from-scratch/stage_2.sh
@@ -1,30 +1,28 @@
 #!/bin/sh
 #
 # stage_2.sh - FreeBSD From Scratch, Stage 2: Ports Installation.
-#              Usage: ./stage_2.sh {configname}
+#              Usage: ./stage_2.sh [-hnp] configname
 #
-# $Id: stage_2.sh,v 1.2 2004-01-03 15:46:31 schweikh Exp $
+# Author:      Jens Schweikhardt
+# $Id: stage_2.sh,v 1.3 2004-01-21 19:39:26 schweikh Exp $
 # $FreeBSD$
 
 DBDIR="/var/db/pkg"
 PORTS="/usr/ports"
+: ${PACKAGES:=${PORTS}/packages}
 LOGDIR="/home/root/setup/ports.log"; mkdir -p ${LOGDIR}
-
-# The linprocfs appears to be required for certain java/jdk* ports.
-if mount | grep '^linprocfs'; then
-	: # linprocfs already mounted.
-else
-	mount /compat/linux/proc
-fi
+PKG_PATH="/cdrom/packages/All:/dvd/packages/All"
+PKG=
 
 MYNAME="$(basename $0)"
 usage () {
 	exec >&2
-	echo "usage: ${MYNAME} [-hn] configname"
+	echo "usage: ${MYNAME} [-hnp] configname"
 	echo ""
 	echo "  Options:"
 	echo "  -h    Print this help text."
 	echo "  -n    Dryrun: just show what would be done."
+	echo "  -p    Install a precompiled package if one can be found."
 	echo ""
 	echo "  The config file (stage_2.conf.configname) is a list of"
 	echo "  ports to install with one entry per line. Each line"
@@ -34,15 +32,38 @@ usage () {
 	exit 1
 }
 
-args=`getopt hn $*`
+# Look for a package in these locations in sequence.
+# Returns as soon as the first is found. Result on stdout.
+#
+#   ${PORTS}/${CATEGORY}/${NAME}
+#   ${PACKAGES}/All
+#   ${PACKAGES}/${CATEGORY}
+#   ${PKG_PATH}
+#
+find_package () {
+	echo "${PORTS}/${CATEGORY}/${NAME}:${PACKAGES}/All:${PACKAGES}/${CATEGORY}:${PKG_PATH}" |
+	tr : '\n' |
+	while read d; do
+		test -d "${d}" || continue
+		PKG=$(ls ${d}/${PKGNAME}.* 2>/dev/null)
+		test $? -eq 0 && echo "${PKG}" && return
+	done
+}
+
+#
+# Parse command line arguments.
+#
+args=`getopt hnp $*`
 if test $? != 0; then
 	usage
 fi
 set -- $args
 DRYRUN=
+CHKPKG=
 for i; do
 	case "$i" in
 	-n) DRYRUN="yes"; shift;;
+	-p) CHKPKG="yes"; shift;;
 	--) shift; break;;
 	*) usage;;
 	esac
@@ -53,6 +74,9 @@ else
 	usage
 fi
 
+#
+# Loop over the ports list.
+#
 while read CATEGORY NAME CMD; do
 	case "${CATEGORY}" in
 	\#*) continue;;
@@ -65,16 +89,21 @@ while read CATEGORY NAME CMD; do
 	fi
 	cd ${DIR}
 	PKGNAME=`make -V PKGNAME`
+	if test -n "${CHKPKG}"; then
+		PKG=$(find_package)
+	else
+		PKG=""
+	fi
 	if test -d "${DBDIR}/${PKGNAME}"; then
 		echo "${CATEGORY}/${NAME} already installed as ${PKGNAME}"
 		continue
 	fi
 	LOG="${LOGDIR}/${CATEGORY}+${NAME}"
 	echo "===> Installing ${CATEGORY}/${NAME}; logging to ${LOG}"
-	test -n "${CMD}" || CMD="make install < /dev/null"
+	test -n "${CMD}" || CMD="make package-recursive < /dev/null"
 	if test -n "${DRYRUN}"; then
-		if ls ${PKGNAME}.* >/dev/null 2>&1; then
-			echo pkg_add -v ${PKGNAME}.*
+		if test -n "${PKG}"; then
+			echo pkg_add -v ${PKG}
 		else
 			echo "${CMD}"
 		fi
@@ -82,12 +111,12 @@ while read CATEGORY NAME CMD; do
 	fi
 	date "++++ Started %v %T +++" > ${LOG}
 	STARTED=$(date +%s)
-	echo "CMD: ${CMD}" >> ${LOG}
 	(
-		if ls ${PKGNAME}.* >/dev/null 2>&1; then
-			echo "Found package" ${PKGNAME}.*
-			pkg_add -v ${PKGNAME}.*
+		if test -n "${PKG}"; then
+			echo "Found package ${PKG}"
+			pkg_add -v ${PKG}
 		else
+			echo "CMD: ${CMD}"
 			make clean
 			eval "${CMD}"
 			make clean # Uncomment if diskspace is tight under ${PORTS}.
