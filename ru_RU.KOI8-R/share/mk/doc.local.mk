d7025bf88e7e8c485a2101c5e10e1bc8acf541e3
den <den@FreeBSD.org>
2004-08-10 08:44:36 +0000

    Add FreeBSD Russian Documentation Project headers

--- ru_RU.KOI8-R/share/mk/doc.local.mk
+++ ru_RU.KOI8-R/share/mk/doc.local.mk
@@ -1,3 +1,10 @@
+#
+# The FreeBSD Russian Documentation Project
+#
 # $FreeBSD$
+# $FreeBSDru: frdp/doc/ru_RU.KOI8-R/share/mk/doc.local.mk,v 1.1 2004/08/10 08:42:59 den Exp $
+#
+# Original revision: 1.1
+#
 
 SP_ENCODING?=	${LANGCODE:C,^.*\.,,}
