83f3e33aa011c2530b978abcbb009bb95c57f5fb
ceri <ceri@FreeBSD.org>
2005-02-12 13:13:14 +0000

    Follow revision 1.5 of www/share/sgml/includes.header.sgml: 	Use less ambiguous content for the ALT attributes in the image 	map.

--- en/cgi/cgi-style.pl
+++ en/cgi/cgi-style.pl
@@ -1,4 +1,4 @@
-# $FreeBSD: www/en/cgi/cgi-style.pl,v 1.17 2000/12/29 09:24:40 knu Exp $
+# $FreeBSD: www/en/cgi/cgi-style.pl,v 1.18 2004/10/13 10:55:55 keramida Exp $
 #
 # Perl routines to encapsulate various elements of HTML page style.
 
@@ -32,15 +32,15 @@ if (!defined($hsty_date)) {
 
 $i_topbar = "<IMG SRC=\"$hsty_base/gifs/bar.gif\" ALT=\"Navigation Bar\" WIDTH=\"565\" HEIGHT=\"33\" BORDER=0 usemap=\"#bar\">
 <map name=\"bar\">
-<area shape=\"rect\" coords=\"1,1,111,31\" href=\"$hsty_base/index.html\" ALT=\"Top\">
+<area shape=\"rect\" coords=\"1,1,111,31\" href=\"$hsty_base/index.html\" ALT=\"Site Root\">
 <area shape=\"rect\" coords=\"112,11,196,31\" href=\"$hsty_base/ports/index.html\" ALT=\"Applications\">
 <area shape=\"rect\" coords=\"196,12,257,33\" href=\"$hsty_base/support.html\" ALT=\"Support\">
 <area shape=\"rect\" coords=\"256,12,365,33\" href=\"$hsty_base/docs.html\" ALT=\"Documentation\"> 
 <area shape=\"rect\" coords=\"366,13,424,32\" href=\"$hsty_base/commercial/commercial.html\" ALT=\"Vendors\">
 <area shape=\"rect\" coords=\"425,16,475,32\" href=\"$hsty_base/search/search.html\" ALT=\"Search\">
-<area shape=\"rect\" coords=\"477,16,516,33\" href=\"$hsty_base/search/index-site.html\" ALT=\"Index\">
-<area shape=\"rect\" coords=\"516,15,562,33\" href=\"$hsty_base/index.html\" ALT=\"Top\">
-<area shape=\"rect\" href=\"$hsty_base/index.html\" coords=\"0,0,564,32\" ALT=\"Top\">
+<area shape=\"rect\" coords=\"477,16,516,33\" href=\"$hsty_base/search/index-site.html\" ALT=\"Site Index\">
+<area shape=\"rect\" coords=\"516,15,562,33\" href=\"$hsty_base/index.html\" ALT=\"Site Root\">
+<area shape=\"rect\" href=\"$hsty_base/index.html\" coords=\"0,0,564,32\" ALT=\"Site Root\">
 </map>";
 
 if (!defined($hsty_home)) {
