1301abfaeda030fc0c417b994f1fdca23b9b19a1
rushani <rushani@ba62deef-0ea0-e111-9166-001e4f0fb9b1>
2006-03-13 16:46:15 +0000

    - Merge the following from the English version:
      1.42  -> 1.47  articles/Makefile
      1.503 -> 1.506 articles/contributing/article.sgml
      1.9   -> 1.10  articles/contributors/Makefile
      1.436 -> 1.446 articles/contributors/article.sgml
      New   -> 1.3   articles/fbsd-from-scratch/stage_1.conf.default
      1.1   -> 1.5   articles/fbsd-from-scratch/stage_1.sh
      New   -> 1.3   articles/fbsd-from-scratch/stage_2.conf.default
      1.1   -> 1.5   articles/fbsd-from-scratch/stage_2.sh
      1.1   -> 1.4   articles/fbsd-from-scratch/stage_3.mk
      1.27  -> 1.30  articles/fonts/article.sgml
      1.23  -> 1.32  articles/problem-reports/article.sgml
    
    - Append stage_[12].conf.default into EXTRAS.
    
                     articles/fbsd-from-scratch/Makefile
    
    Submitted by:	Hiroo Ono <hiroo _at_ jp dot FreeBSD dot org>
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/doc/head@27320 ba62deef-0ea0-e111-9166-001e4f0fb9b1

--- ja_JP.eucJP/articles/fbsd-from-scratch/stage_2.sh
+++ ja_JP.eucJP/articles/fbsd-from-scratch/stage_2.sh
@@ -1,114 +1,83 @@
 #!/bin/sh
 #
 # stage_2.sh - FreeBSD From Scratch, 第 2 段階: ports のインストール
-#              使い方: ./stage_2.sh
+#              使い方: ./stage_2.sh [-hnp] configname
 #
+# 著者:      Jens Schweikhardt
+# $Id: stage_2.sh,v 1.2 2006-03-13 16:46:15 rushani Exp $
 # $FreeBSD$
-# Original revision: 1.1
+# Original revision: 1.5
 
-DBDIR=/var/db/pkg
-PORTS=/usr/ports
-LOGDIR=/home/root/setup/ports.log; mkdir -p ${LOGDIR}
+DBDIR="/var/db/pkg"
+PORTS="/usr/ports"
+: ${PACKAGES:=${PORTS}/packages}
+LOGDIR="/home/root/setup/ports.log"; mkdir -p ${LOGDIR}
+PKG_PATH="/cdrom/packages/All:/dvd/packages/All"
+PKG=
 
-# 複数の port で使用される環境変数の設定
-PAPERSIZE=a4;    export PAPERSIZE
-USA_RESIDENT=NO; export USA_RESIDENT
-
-MYNAME=$(basename $0)
+MYNAME="$(basename $0)"
 usage () {
 	exec >&2
-	echo "usage: ${MYNAME} [-hn]"
+	echo "usage: ${MYNAME} [-hnp] configname"
 	echo ""
 	echo "  Options:"
 	echo "  -h    Print this help text."
 	echo "  -n    Dryrun: just show what would be done."
+	echo "  -p    Install a precompiled package if one can be found."
+	echo ""
+	echo "  The config file (stage_2.conf.configname) is a list of"
+	echo "  ports to install with one entry per line. Each line"
+	echo "  consists of two or three space separated fields:"
+	echo "  category, port, and optionally a build command."
 	echo ""
 	exit 1
 }
 
-args=`getopt hn $*`
+# これらの場所にあるパッケージを順に探す。
+# 1 つ見つかり次第戻って、結果を標準出力に表示する。
+#
+#   ${PORTS}/${CATEGORY}/${NAME}
+#   ${PACKAGES}/All
+#   ${PACKAGES}/${CATEGORY}
+#   ${PKG_PATH}
+#
+find_package () {
+	echo "${PORTS}/${CATEGORY}/${NAME}:${PACKAGES}/All:${PACKAGES}/${CATEGORY}:${PKG_PATH}" |
+	tr : '\n' |
+	while read d; do
+		test -d "${d}" || continue
+		PKG=$(ls ${d}/${PKGNAME}.* 2>/dev/null)
+		test $? -eq 0 && echo "${PKG}" && return
+	done
+}
+
+#
+# コマンドライン引数を処理する。
+#
+args=`getopt hnp $*`
 if test $? != 0; then
 	usage
 fi
 set -- $args
 DRYRUN=
+CHKPKG=
 for i; do
 	case "$i" in
-	-n) DRYRUN=yes;;
-	--) break;;
+	-n) DRYRUN="yes"; shift;;
+	-p) CHKPKG="yes"; shift;;
+	--) shift; break;;
 	*) usage;;
 	esac
 done
+if test $# -eq 1; then
+	DATAFILE="$1"
+else
+	usage
+fi
 
-cat << EOF |
-lang perl5
-security sudo
-x11-servers XFree86-4-Server
-x11 wrapper
-x11 XFree86-4-libraries
-x11 XFree86-4-clients
-x11-fonts XFree86-4-font75dpi
-x11-fonts XFree86-4-font100dpi
-x11-fonts XFree86-4-fontScalable
-x11-fonts urwfonts
-x11-fonts webfonts
-x11-toolkits open-motif
-x11 rxvt
-x11-wm ctwm
-security openssh-askpass
-astro xplanet
-astro setiathome make BATCH=yes install
-astro xephem
-editors vim
-print ghostscript-gnu make A4=yes BATCH=yes install
-print a2ps-a4
-print psutils-a4
-print gv
-print acroread5
-print transfig
-archivers zip
-archivers unzip
-java linux-sun-jdk13 yes | make install
-java jdk13
-www apache2
-www weblint
-www amaya
-www mozilla make WITHOUT_MAILNEWS=yes WITHOUT_CHATZILLA=yes install
-www netscape48-navigator
-www checkbot
-www privoxy
-graphics xfig
-graphics xv
-graphics fxtv
-lang expect
-news tin
-net freebsd-uucp
-net cvsup-without-gui
-net pathchar make NO_CHECKSUM=yes install
-ftp wget
-ftp ncftp3
-textproc ispell
-german ispell-neu
-german ispell-alt
-textproc docproj make JADETEX=yes HAVE_MOTIF=yes install
-sysutils samefile
-sysutils pstree
-sysutils mkisofs
-sysutils cdrtools
-sysutils grub
-devel ddd
-devel ctags
-devel ElectricFence
-mail procmail make BATCH=yes install
-mail metamail
-mail mutt
-mail spamoracle
-emulators mtools
-sysutils portupgrade
-news inn-stable CONFIGURE_ARGS="--enable-uucp-rnews --enable-setgid-inews" make install
-misc figlet-fonts
-textproc gmat
-EOF
+#
+# ports 一覧に対して繰り返す。
+#
 while read CATEGORY NAME CMD; do
 	case "${CATEGORY}" in
 	\#*) continue;;
@@ -121,29 +90,43 @@ while read CATEGORY NAME CMD; do
 	fi
 	cd ${DIR}
 	PKGNAME=`make -V PKGNAME`
+	if test -n "${CHKPKG}"; then
+		PKG=$(find_package)
+	else
+		PKG=""
+	fi
 	if test -d "${DBDIR}/${PKGNAME}"; then
 		echo "${CATEGORY}/${NAME} already installed as ${PKGNAME}"
 		continue
 	fi
 	LOG="${LOGDIR}/${CATEGORY}+${NAME}"
 	echo "===> Installing ${CATEGORY}/${NAME}; logging to ${LOG}"
-	test -n "${CMD}" || CMD="make install"
+	test -n "${CMD}" || CMD="make install BATCH=yes < /dev/null"
 	if test -n "${DRYRUN}"; then
-		echo "${CMD}"
+		if test -n "${PKG}"; then
+			echo pkg_add -v ${PKG}
+		else
+			echo "${CMD}"
+		fi
 		continue
 	fi
-	date "++++++++++ %v %T +++++++++" > ${LOG}
-	echo "CMD: ${CMD}" >> ${LOG}
+	date "++++ Started %v %T +++" > ${LOG}
+	STARTED=$(date +%s)
 	(
-		make clean
-		eval "${CMD}"
-		# make clean # ${PORTS} 以下のディスク容量がすくなければコメントをはずす
+		if test -n "${PKG}"; then
+			echo "Found package ${PKG}"
+			pkg_add -v ${PKG}
+		else
+			echo "CMD: ${CMD}"
+			make clean
+			eval "${CMD}"
+			make clean # ${PORTS} 以下のディスク容量がすくなければコメントをはずす
+		fi
 	) 2>&1 | tee -a ${LOG}
-done
+	FINISHED=$(date +%s)
+	DURATION=$(dc -e "${FINISHED} ${STARTED} - p")
+	date "++++ Finished %v %T after ${DURATION} secs +++" >> ${LOG}
+done < stage_2.conf.${DATAFILE}
 
-# StarOffice は、X11 を使った対話的インストールが必要なので、
-# 古いシステム上で "make package" を実行して作成した
-# package からインストールする。
-#pkg_add ${PORTS}/editors/staroffice52/staroffice-*.tbz
-
-# EOF $RCSfile: stage_2.sh,v $    vim: tabstop=4:
+# vim: tabstop=4:
+# EOF $RCSfile: stage_2.sh,v $
