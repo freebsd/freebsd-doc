6be9b78bd8e02357c40fb5fcc6f57bb37e73cbcb
Hideyuki KURASHINA <rushani@FreeBSD.org>
2006-03-13 16:46:15 +0000

    - Merge the following from the English version:
      1.42  -> 1.47  articles/Makefile
      1.503 -> 1.506 articles/contributing/article.sgml
      1.9   -> 1.10  articles/contributors/Makefile
      1.436 -> 1.446 articles/contributors/article.sgml
      New   -> 1.3   articles/fbsd-from-scratch/stage_1.conf.default
      1.1   -> 1.5   articles/fbsd-from-scratch/stage_1.sh
      New   -> 1.3   articles/fbsd-from-scratch/stage_2.conf.default
      1.1   -> 1.5   articles/fbsd-from-scratch/stage_2.sh
      1.1   -> 1.4   articles/fbsd-from-scratch/stage_3.mk
      1.27  -> 1.30  articles/fonts/article.sgml
      1.23  -> 1.32  articles/problem-reports/article.sgml
    
    - Append stage_[12].conf.default into EXTRAS.
    
                     articles/fbsd-from-scratch/Makefile
    
    Submitted by:	Hiroo Ono <hiroo _at_ jp dot FreeBSD dot org>

--- ja_JP.eucJP/articles/fbsd-from-scratch/stage_1.sh
+++ ja_JP.eucJP/articles/fbsd-from-scratch/stage_1.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 #
 # stage_1.sh - FreeBSD From Scratch, 第 1 段階: システムのインストール
-#              使い方: ./stage_1.sh
+#              使い方: ./stage_1.sh profile
+#              読み込むファイル: ./stage_1.conf.profile
+#              書き出すファイル: ./stage_1.log.profile
 #
+# 著者:      Jens Schweikhardt
+# $Id: stage_1.sh,v 1.2 2006-03-13 16:46:15 rushani Exp $
 # $FreeBSD$
-# Original revision: 1.1
+# Original revision: 1.5
 
-set -x -e
 PATH=/bin:/usr/bin:/sbin:/usr/sbin
 
 # 前提とする環境:
@@ -14,47 +17,22 @@ PATH=/bin:/usr/bin:/sbin:/usr/sbin
 # a) "make buildworld" と "make buildkernel" が正常に終了していること。
 # b) 未使用パーティションがあること (ルートファイルシステム用に少なくとも 1 個、
 #    好みに応じて /usr や /var 用のものを用意する)
+# c) カスタマイズされた stage_1.conf.profile ファイル。
 
-# 新しいシステムを作成する場所を示すルートマウントポイントを指定。
-# マウントポイントとして使われるだけなので、マウントポイントのある
-# ファイルシステムにファイルは置かれず、書き込みはすべてマウントした
-# ファイルシステムに行なわれる。
-DESTDIR=/newroot
-SRC=/usr/src         # src ツリーのある場所
+if test $# -ne 1; then
+  echo "usage: stage_1.sh profile" 1>&2
+  exit 1
+fi
 
 # ---------------------------------------------------------------------------- #
 # ステップ 1: $DESTDIR 以下に空のディレクトリツリーを作成
 # ---------------------------------------------------------------------------- #
 
 step_one () {
-  # 新しいルートファイルシステムを作成する。必須。
-  # デバイス名 (DEV_*) を変更すること。変更しないとシステムが壊れる危険性がある。
-  DEV_ROOT=/dev/da3s1a
-  mkdir -p ${DESTDIR}
-  newfs ${DEV_ROOT}
-  tunefs -n enable ${DEV_ROOT}
-  mount -o noatime ${DEV_ROOT} ${DESTDIR}
-
-  # その他のファイルシステムと初期マウントポイント。オプション。
-  DEV_VAR=/dev/vinum/var_a
-  newfs ${DEV_VAR}
-  tunefs -n enable ${DEV_VAR}
-  mkdir -m 755 ${DESTDIR}/var
-  mount -o noatime ${DEV_VAR} ${DESTDIR}/var
-
-  DEV_USR=/dev/vinum/usr_a
-  newfs ${DEV_USR}
-  tunefs -n enable ${DEV_USR}
-  mkdir -m 755 ${DESTDIR}/usr
-  mount -o noatime ${DEV_USR} ${DESTDIR}/usr
-
-  mkdir -m 755 -p ${DESTDIR}/usr/ports
-  mount /dev/vinum/ports ${DESTDIR}/usr/ports
+  create_file_systems
 
   # ここで他のすべてのディレクトリを作成。必須。
   cd ${SRC}/etc; make distrib-dirs DESTDIR=${DESTDIR}
-  # 個人的には tmp -> var/tmp とシンボリックリンクを張るのが好み。オプション。
-  cd ${DESTDIR}; rmdir tmp; ln -s var/tmp
 }
 
 # ---------------------------------------------------------------------------- #
@@ -62,48 +40,21 @@ step_one () {
 # ---------------------------------------------------------------------------- #
 
 step_two () {
-  # 好みに応じて、このリストに追加・削除すること。ほとんどの場合は必須。
-  for f in \
-    /.profile \
-    /etc/group \
-    /etc/hosts \
-    /etc/inetd.conf \
-    /etc/ipfw.conf \
-    /etc/make.conf \
-    /etc/master.passwd \
-    /etc/nsswitch.conf \
-    /etc/ntp.conf \
-    /etc/printcap \
-    /etc/profile \
-    /etc/rc.conf \
-    /etc/resolv.conf \
-    /etc/start_if.xl0 \
-    /etc/ttys \
-    /etc/ppp/* \
-    /etc/mail/aliases \
-    /etc/mail/aliases.db \
-    /etc/mail/hal9000.mc \
-    /etc/mail/service.switch \
-    /etc/ssh/*key* \
-    /etc/ssh/*_config \
-    /etc/X11/XF86Config-4 \
-    /boot/splash.bmp \
-    /boot/loader.conf \
-    /boot/device.hints ; do
-    cp -p ${f} ${DESTDIR}${f}
-  done
+  copy_files
+
   # mergemaster の作業ファイルがあれば削除。
   TEMPROOT=/var/tmp/temproot.stage1
   if test -d ${TEMPROOT}; then
     chflags -R 0 ${TEMPROOT}
     rm -rf ${TEMPROOT}
   fi
+  export MAKEDEVPATH="/bin:/sbin:/usr/bin"
   mergemaster -i -m ${SRC}/etc -t ${TEMPROOT} -D ${DESTDIR}
   cap_mkdb ${DESTDIR}/etc/login.conf
   pwd_mkdb -d ${DESTDIR}/etc -p ${DESTDIR}/etc/master.passwd
 
   # mergemaster は /var/log に置かれる空ファイルを作成しないので、
-  # ここで作成。ただし上のループでコピーされている場合は、それを使う。
+  # ここで作成。ただし copy_files でコピーされている場合は、それを使う。
   cd ${TEMPROOT}
   find . -type f | sed 's,^\./,,' |
   while read f; do
@@ -125,6 +76,13 @@ step_two () {
 step_three () {
   cd ${SRC}
   make installworld DESTDIR=${DESTDIR}
+
+  # 追加の互換ライブラリをインストールする (オプション)。libc.so.4 を
+  # 動的リンクするプログラムがあれば、つまり、
+  # /usr/libexec/ld-elf.so.1: Shared object "libc.so.4" not found
+  # というエラーメッセージが見つかったら、これを利用すること。
+  cd lib/compat/compat4x.i386
+  make all install DESTDIR=${DESTDIR}
 }
 
 # ---------------------------------------------------------------------------- #
@@ -137,89 +95,41 @@ step_four () {
   # ステップ 2 でコピーしていなければ、次の 2 行を使ってコピーすること。
   #   cp sys/boot/forth/loader.conf ${DESTDIR}/boot/defaults
   #   cp sys/i386/conf/GENERIC.hints ${DESTDIR}/boot/device.hints
-  make installkernel DESTDIR=${DESTDIR} KERNCONF=HAL9000
+  make installkernel DESTDIR=${DESTDIR} KERNCONF=${KERNCONF}
 }
 
 # ---------------------------------------------------------------------------- #
-# ステップ 5: 必須のファイルのインストールと変更
+# ステップ 5: /etc/fstab とタイムゾーン情報のインストール
 # ---------------------------------------------------------------------------- #
 
 step_five () {
-  # /etc/fstab の作成。必須。自分のデバイスに合うように変更すること。
-  cat <<EOF >${DESTDIR}/etc/fstab
-# Device         Mountpoint          FStype    Options              Dump Pass#
-/dev/da3s1b      none                swap      sw                   0    0
-/dev/da4s2b      none                swap      sw                   0    0
-/dev/da3s1a      /                   ufs       rw                   1    1
-/dev/da1s2a      /src                ufs       rw                   0    2
-/dev/da2s2f      /share              ufs       rw                   0    2
-/dev/vinum/var_a /var                ufs       rw                   0    2
-/dev/vinum/usr_a /usr                ufs       rw                   0    2
-/dev/vinum/home  /home               ufs       rw                   0    2
-/dev/vinum/ncvs  /home/ncvs          ufs       rw,noatime           0    2
-/dev/vinum/ports /usr/ports          ufs       rw,noatime           0    2
-#
-/dev/cd0         /dvd                cd9660    ro,noauto            0    0
-/dev/cd1         /cdrom              cd9660    ro,noauto            0    0
-proc             /proc               procfs    rw                   0    0
-EOF
+  create_etc_fstab
 
-  # その他のディレクトリ。オプション。
-  mkdir -m 755 -p ${DESTDIR}/src;       chown root:wheel ${DESTDIR}/src
-  mkdir -m 755 -p ${DESTDIR}/share;     chown root:wheel ${DESTDIR}/share
-  mkdir -m 755 -p ${DESTDIR}/dvd;       chown root:wheel ${DESTDIR}/dvd
-  mkdir -m 755 -p ${DESTDIR}/home;      chown root:wheel ${DESTDIR}/home
-  mkdir -m 755 -p ${DESTDIR}/usr/ports; chown root:wheel ${DESTDIR}/usr/ports
   # タイムゾーンの設定。ほとんどの場合は必須。
-  cp ${DESTDIR}/usr/share/zoneinfo/Europe/Berlin ${DESTDIR}/etc/localtime
+  cp ${DESTDIR}/usr/share/zoneinfo/${TIMEZONE} ${DESTDIR}/etc/localtime
   if test -r /etc/wall_cmos_clock; then
-     cp -p /etc/wall_cmos_clock ${DESTDIR}/etc/wall_cmos_clock
+    cp -p /etc/wall_cmos_clock ${DESTDIR}/etc/wall_cmos_clock
   fi
 }
 
 # ---------------------------------------------------------------------------- #
-# ステップ 6: 新しいシステムにログインする時に重要な内容
-# 注意: あまり多くのバイナリをこの時点でインストールしないこと。稼働している
-# 古いシステムと、インストールした新しいバイナリ・ヘッダを組み合わせると、
-# ブートストラップ問題に陥る可能性がある。ports は新しいシステムが起動した後に
-# 再構築する方がよい。
+# ステップ 6: 残りのカスタマイズ
 # ---------------------------------------------------------------------------- #
 
 step_six () {
-  chroot ${DESTDIR} sh -c "cd /usr/ports/shells/zsh; make clean install clean"
-  chroot ${DESTDIR} sh -c "cd /etc/mail; make install"  # configure sendmail
-
-  # compat シンボリックリンクがないと、linux_base のファイル群が
-  # ルートファイルシステムに置かれてしまう。
-  cd ${DESTDIR}; mkdir -m 755 usr/compat
-  chown root:wheel usr/compat; ln -s usr/compat
-  mkdir -m 755 usr/compat/linux
-  mkdir -m 755 boot/grub
-
-  # /etc/printcap で指定したスプールディレクトリを作成。
-  cd ${DESTDIR}/var/spool/output/lpd; mkdir -p as od ev te lp da
-  touch ${DESTDIR}/var/log/lpd-errs
-
-  # 古いシステムから引き継ぎたいファイルを指定
-  for f in \
-    /var/cron/tabs/root \
-    /var/mail/* \
-    /boot/grub/*; do
-    cp -p ${f} ${DESTDIR}${f}
-  done
-
-  # 共有パーティション /home がなければ、/home をコピーした方がよいかも知れない。
-  # mkdir -p ${DESTDIR}/home
-  # cd /home; tar cf - . | (cd ${DESTDIR}/home; tar xpvf -)
-
-  # FreeBSD 5.x より perl は /usr/local/bin に置かれるようになったが、
-  # 多くのスクリプトは #!/usr/bin/perl でハードコードされている。
-  # これらを動作させるため、シンボリックリンクを作成しておく。
-  cd ${DESTDIR}/usr/bin; ln -s ../local/bin/perl
-  cd ${DESTDIR}/usr; rmdir src; ln -s ../src/current src
+  all_remaining_customization
 }
 
 do_steps () {
+  echo "PROFILE=${PROFILE}"
+  echo "DESTDIR=${DESTDIR}"
+  echo "SRC=${SRC}"
+  echo "KERNCONF=${KERNCONF}"
+  echo "TIMEZONE=${TIMEZONE}"
+  echo "TYPE=${TYPE}"
+  echo "REVISION=${REVISION}"
+  echo "BRANCH=${BRANCH}"
+  echo "RELDATE=${RELDATE}"
   step_one
   step_two
   step_three
@@ -228,6 +138,37 @@ do_steps () {
   step_six
 }
 
-do_steps 2>&1 | tee stage_1.log
+# ---------------------------------------------------------------------------- #
+# ここから実行開始
+# ---------------------------------------------------------------------------- #
 
-# EOF $RCSfile: stage_1.sh,v $    vim: tabstop=2:expandtab:
+PROFILE="$1"
+set -x -e -u # エラーが発生するか未定義変数を使用したら停止する。
+. ./stage_1.conf.${PROFILE}
+
+# world を make するのに使われたソースコードから変数をいくつか決定する。
+# この変数は、たとえば 4.x と 5.x どちらのシステムをインストールするの
+# かといった動作を変更するのに使われる。RELDATE に対する
+# __FreeBSD_version は Port 作成者のためのハンドブック (Porter's Handbook)
+# で説明されている。
+# doc/en_US.ISO8859-1/books/porters-handbook/freebsd-versions.html
+# 日本語版もあるが、最新の情報は英語版を参照のこと。
+# doc/ja_JP.eucJP/books/porters-handbook/freebsd-versions.html
+# 形式は、<メジャー番号><マイナー番号 2 桁><リリースブランチなら 0, それ以外は 1>xx
+# 結果は次のようなものになる。
+#
+#   TYPE="FreeBSD"
+#   REVISION="4.9"
+#   BRANCH="RC"      { "CURRENT", "STABLE", "RELEASE" }
+#   RELDATE="502101"
+#
+eval $(awk '/^(TYPE|REVISION|BRANCH)=/' ${SRC}/sys/conf/newvers.sh)
+RELDATE=$(awk '/^[ \t]*#[ \t]*define[ \t][ \t]*__FreeBSD_version[ \t]/ {
+                print $3
+              }' ${SRC}/sys/sys/param.h)
+
+echo "=> Logging to stage_1.log.${PROFILE}"
+do_steps 2>&1 | tee stage_1.log.${PROFILE}
+
+# vim: tabstop=2:expandtab:shiftwidth=2:
+# EOF $RCSfile: stage_1.sh,v $
