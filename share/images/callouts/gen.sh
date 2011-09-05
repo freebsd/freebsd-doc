283a16c3b0a57e69981f2adf5cb1f250f163bfc9
Doug Barton <dougb@FreeBSD.org>
2011-09-05 23:05:31 +0000

    Make the background transparent [1]
    Slight tweaks to the font size and kerning
    
    Submitted by:	Igor Mozolevsky <igor@hybrid-lab.co.uk> [1]

--- share/images/callouts/gen.sh
+++ share/images/callouts/gen.sh
@@ -4,5 +4,6 @@
 
 for i in `seq 1 30`
 do
-    convert -size 101x101 xc:white -fill black -draw 'circle 50,50 50' -fill white -stroke none -pointsize 85 -gravity center -kerning -1 -font Helvetica-bold -draw "text 0,5 \"$i\"" -scale '12x12' $i.png
+    convert -size 101x101 xc:green -transparent green -fill black -draw 'circle 50,50 50' -fill white -stroke none -pointsize 83 -gravity center -kerning -5 -font Helvetica-bold -draw "text 0,5 \"$i\"" -scale '12x12' $i.png
+
 done
