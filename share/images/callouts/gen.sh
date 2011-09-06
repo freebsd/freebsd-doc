9c19a44a8328ebd563965d70ec39542569edfea5
Doug Barton <dougb@FreeBSD.org>
2011-09-06 01:21:48 +0000

    Add a note about where to edit %callout-graphics-number-limit% [1]
    Split the generation of images up to use a larger font on 1-9, and
    smaller on 10-30 to further improve the appearance.
    
    s/seq/jot/ so users on < 9.x can run this script
    
    Submitted by:	gjb [1]

--- share/images/callouts/gen.sh
+++ share/images/callouts/gen.sh
@@ -2,8 +2,17 @@
 
 # $FreeBSD$
 
-for i in `seq 1 30`
-do
-    convert -size 101x101 xc:green -transparent green -fill black -draw 'circle 50,50 50' -fill white -stroke none -pointsize 83 -gravity center -kerning -5 -font Helvetica-bold -draw "text 0,5 \"$i\"" -scale '12x12' $i.png
+# For more than 30 adjust %callout-graphics-number-limit%
+# in doc/share/sgml/freebsd-html.dsl
 
+for i in `jot 9 1`
+do
+    convert -size 101x101 xc:green -transparent green -fill black -draw 'circle 50,50 50' -fill white -stroke none -pointsize 90 -gravity center -kerning -5 -font Helvetica-bold -draw "text 0,5 \"$i\"" -scale '12x12' $i.png
 done
+
+for i in `jot 21 10`
+do
+    convert -size 101x101 xc:green -transparent green -fill black -draw 'circle 50,50 50' -fill white -stroke none -pointsize 80 -gravity center -kerning -5 -font Helvetica-bold -draw "text 0,5 \"$i\"" -scale '12x12' $i.png
+done
+
+exit 0
