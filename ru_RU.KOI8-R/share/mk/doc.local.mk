78cf7f49ff5c43df62ea2047d3a239ba012f58d6
Taras Korenko <taras@FreeBSD.org>
2012-05-25 14:40:55 +0000

    + Correct 'Original revision' references: make them point to the corresponding SVN revisions.

--- ru_RU.KOI8-R/share/mk/doc.local.mk
+++ ru_RU.KOI8-R/share/mk/doc.local.mk
@@ -4,7 +4,7 @@
 # $FreeBSD$
 # $FreeBSDru: frdp/doc/ru_RU.KOI8-R/share/mk/doc.local.mk,v 1.1 2004/08/10 08:42:59 den Exp $
 #
-# Original revision: 1.1
+# Original revision: r21884
 #
 
 SP_ENCODING?=	${LANGCODE:C,^.*\.,,}
