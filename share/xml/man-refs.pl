fc3c07a9b110bee1ca3c8b8915335fb4928428f8
mat <mat@FreeBSD.org>
2016-11-08 13:13:28 +0000

    This has not been updated in a while, catch up with the current format.

--- share/xml/man-refs.pl
+++ share/xml/man-refs.pl
@@ -7,5 +7,5 @@ while (<>) {
         next unless (m,^(.*/)([\w\._-]+)\.(\d\w*)(\.gz)?$,);
         my ($entity, $page, $volume) = ($2, $2, $3);
         $entity =~ y/_/./;
-        print "<!ENTITY man.$entity.$volume \"<citerefentry/<refentrytitle/$page/<manvolnum/$volume//\">\n";
+        print "<!ENTITY man.$entity.$volume \"<citerefentry xmlns='http://docbook.org/ns/docbook'><refentrytitle>$page</refentrytitle><manvolnum>$volume</manvolnum></citerefentry>\">\n";
 }
