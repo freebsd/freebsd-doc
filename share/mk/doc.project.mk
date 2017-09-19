6656dadf618b5d7cac1c6b9dd4fce522d11c88af
wosch <wosch@c2e8774f-c49f-e111-b436-862b2bbc8956>
2017-09-19 10:03:26 +0000

    Implement workaround for a parallel doc build
    $ make p-all
    
    It works for the sub-directories in a given directory.
    
    On a standard quad core machine the build time goes down
    from ca. 20min to 5 minutes.
    
    PR: 222322
    
    
    git-svn-id: svn+ssh://svn.freebsd.org/doc/head@50880 c2e8774f-c49f-e111-b436-862b2bbc8956

--- share/mk/doc.project.mk
+++ share/mk/doc.project.mk
@@ -104,3 +104,9 @@ DOC_LOCAL_MK=	${DOC_PREFIX}/${LANGCODE}/share/mk/doc.local.mk
 
 # Subdirectory glue.
 .include "doc.subdir.mk"
+
+# parallel build for target "all"
+NCPU?= 8
+p-all:
+	make -V SUBDIR | sed -E 's/[ ]+$$//' | tr " " "\n" | sed -E 's/^/make -C /' | tr '\n' '\0' | xargs -0 -n1 -P${NCPU} /bin/sh -c
+
