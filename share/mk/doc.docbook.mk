f39d997cd0b9189b16fea143066d7daf21a2fbec
blackend <blackend@FreeBSD.org>
2018-04-02 12:45:43 +0000

    Update maxdepth from 6000 to 12000 [1].  This should fix the doc related ports build on FreeBSD 11.X.
    PR:	225287
    Submitted by:	Keren Sky <keren_sky@live.com>
    Obtained from:	Kenji Rikitake <kenji@k2r.org> [1]

--- share/mk/doc.docbook.mk
+++ share/mk/doc.docbook.mk
@@ -234,7 +234,7 @@ CLEANFILES+= ${DOC}.aux ${DOC}.dvi ${DOC}.log ${DOC}.out ${DOC}.tex-pdf ${DOC}.t
 .if ${RENDERENGINE} == "fop"
 XSLTPROCOPTS+=	--param img.src.path "'${IMGDIR}/'"
 XSLTPROCOPTS+=	--param callout.graphics.path "'${CALLOUTDIR}/'"
-XSLTPROCOPTS+=	--maxdepth 6000
+XSLTPROCOPTS+=	--maxdepth 12000
 .endif
 .for _curimage in ${LOCAL_IMAGES_EPS:M*share*}
 CLEANFILES+= ${_curimage:T} ${_curimage:H:T}/${_curimage:T}
