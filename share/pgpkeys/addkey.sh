b25f5543cbf3ce11cff3861dec234273b28f211f
0mp <0mp@FreeBSD.org>
2018-08-14 20:23:19 +0000

    Make addkey.sh work with usernames starting with a digit.
    Reviewed by:	des, krion
    Approved by:	krion (mentor)
    Differential Revision:	https://reviews.freebsd.org/D16549

--- share/pgpkeys/addkey.sh
+++ share/pgpkeys/addkey.sh
@@ -65,7 +65,7 @@ fi
 if [ -z "${me}" ] ; then
 	error "Unable to determine user name."
 fi
-if ! expr "${me}" : '^[a-z][0-9a-z-]*$' >/dev/null ; then
+if ! expr "${me}" : '^[0-9a-z][0-9a-z-]*$' >/dev/null ; then
 	error "${me} does not seem like a valid user name."
 fi
 
