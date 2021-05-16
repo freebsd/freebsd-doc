5a9f8ba84024192b35746faa2a665f2e79571ed0
Sergio Carlavilla Delgado <carlavilla@FreeBSD.org>
2021-05-16 22:41:00 +0200

    Add new tool to generate the pgpkeys.txt file

---
+++ documentation/tools/global-pgpkeys-creator.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+
+=begin
+
+BSD 2-Clause License
+Copyright (c) 2020-2021, The FreeBSD Project
+Copyright (c) 2020-2021, Sergio Carlavilla <carlavilla@FreeBSD.org>
+
+This script will merge all the pgpkeys into one single file
+
+=end
+
+def getAllPGPKeys()
+  return Dir.glob('./static/pgpkeys/*.key').sort
+end
+
+def processAllPGPKeys(keysFiles, pgpKeysFile)
+  keysFiles.each{ |keyFile|
+    processPGPKey(keyFile, pgpKeysFile)
+  }
+end
+
+def processPGPKey(keyFile, pgpKeysFile)
+  File.readlines(keyFile).each do |line|
+    pgpKeysFile.puts(line)
+  end
+end
+
+# Main method
+keysFiles = getAllPGPKeys()
+
+pgpKeysFile = File.new("./static/pgpkeys/pgpkeys.txt", "w")
+
+processAllPGPKeys(keysFiles, pgpKeysFile)
