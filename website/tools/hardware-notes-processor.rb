b2ff99423bdb0dd72c7ce2e07d26a742c793edc0
Sergio Carlavilla Delgado <carlavilla@FreeBSD.org>
2021-12-26 19:54:18 +0100

    Add Hugo Archetype to generate new releases.
    Use:
    
    Navigate to website root folder and execute `generate-release` to
    generate the 15.0 release using the main branch:
    
    * make generate-release RELEASE=15.0 MAIN=true
    
    Navigate to website root folder and execute `generate-release`
    to generate the 12.3 release using the 12.3 release branch:
    
    * make generate-release RELEASE=12.3

---
+++ website/tools/hardware-notes-processor.rb
@@ -0,0 +1,44 @@
+#!/usr/bin/env ruby
+=begin
+
+BSD 2-Clause License
+Copyright (c) 2020-2021, The FreeBSD Project
+Copyright (c) 2020-2021, Sergio Carlavilla <carlavilla@FreeBSD.org>
+
+This script will generate the hardware notes
+
+=end
+require 'open3'
+
+# Main method
+puts "Processing hardware notes..."
+
+if ARGV.length < 1 || ARGV.length > 1
+  puts "Only expecting the release version"
+  exit
+end
+
+hardwareNotesPath = ARGV[0]
+hardwareNotesContent = ""
+
+File.foreach(hardwareNotesPath).with_index do |line|
+  if (line[/&hwlist.\b/])
+    manualPage = line.gsub("&hwlist.", "").gsub(";", "").gsub("\n", "")
+
+    if(File.exist?("tmp/share/man/man4/" + manualPage + ".4"))
+      cmd = "mandoc -Tmarkdown tmp/share/man/man4/" + manualPage + ".4 | sed -n '/# HARDWARE/,/#/{/#/!p;}' "
+      mandocOut, err, s = Open3::capture3(cmd)
+      if s.success?
+        hardwareNotesContent << mandocOut
+      end
+    else
+      puts "WARNING: The manual page " + manualPage + " does not exists"
+    end
+  else
+    hardwareNotesContent << line
+  end
+end
+
+File.open(hardwareNotesPath, 'w') do |out|
+  out << hardwareNotesContent
+end
